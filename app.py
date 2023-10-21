ce06d9151cf4f841caea5d053bb19a7a62ee25e3
suptts <suptts2023@email.com>
2023-10-21 20:28:10 +0700

    add file

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
 
+def get_pdf_text(pdf_doc):
+    text = ""
+    for pdf in pdf_doc:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text +=page.extract_text()
+    return text
   
 def main():
-    #print("hello world")    
-    st.set_page_config(page_title="Chat with PDF", page_icon=":books:")
-    st.header("Chat with PDF :books:")
+    #print("hello world")
+    load_dotenv()    
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    st.header("Chat with multiple PDF :books:")
     st.text_input("Ask a question about your documents")
     
     with st.sidebar:
-        st.subheader("Your document")
-        st.file_uploader("Upload you PDF here and click on 'Prodess'")
-        st.button("Process")
+        st.subheader("Your documents")
+        pdf_doc = st.file_uploader("Upload your PDFs here and click on 'Prodess'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_doc)
+                st.write(raw_text)
+
+                # get the text chunks
+
+                # create vector store
+
 
 if __name__ == '__main__':
     main()
